b75ff8f8088bef4da868edde34acc6fd80c721dd
Can Cui <cancui89@gmail.com>
2018-05-03 22:49:10 +0100

    created some exampkes for interdata analysis from gm,pl,backup

--- main/dataanalysis/interdata_mysql_universe.py
+++ main/dataanalysis/interdata_mysql_universe.py
@@ -1,84 +1,59 @@
 import mysql.connector
-import re
-import random
 
 
+def querymysql(query_command,dbconfig):
+    db=mysql.connector.connect(host=dbconfig[0],
+                           database=dbconfig[1],
+                           user=dbconfig[2],
+                           password=dbconfig[3])
+    cursor=db.cursor()
+    cursor.execute(query_command)
+    queryresult=cursor.fetchall()
+    cursor.close()
+    db.close()
+    return queryresult
 
 userid=14
+lccgameconfig=['localhost','lccgame','host','host']
 
-#-------------------------------------------
-# Connect to MYSQL and pick out the data
-#-------------------------------------------
-def getallinterID(hostname):
-    db=mysql.connector.connect(host=hostname,
-                           database='lccgame',
-                           user='host',
-                           password='host')
-    cursor=db.cursor()
-    cursor.execute("SELECT COMM_ID FROM backup_scalsc_states ")
-    db.close()
-    queryresult=cursor.fetchall()
-    newresult=tuple(set(queryresult))
-    allinterID=( )
-    for i in range(len(newresult)):
-        allinterID=allinterID+(newresult[i][0],)
-    return allinterID
+query="SELECT * FROM lccengine WHERE COMM_ID='INT7254'"
+interdata_raw=querymysql(query,lccgameconfig)
+interdata=[x for x in interdata_raw[0] if x!='']
 
 
-#get an agent's filled LCC protocol(s) under an interaction ID
-def getintagentstates(interactionID):
-    db=mysql.connector.connect(host="localhost",
-                           database='lccgame',
-                           user='host',
-                           password='host')
-    #db=MySQLdb.connect("localhost","host","host","lccgame")
-    cursor=db.cursor()
-    cursor.execute("SELECT STATE FROM backup_scalsc_states WHERE COMM_ID=%r" %interactionID)
-    agentstates=cursor.fetchall()
-    cursor.execute("SELECT PROTOCOL FROM backup_scalsc_states WHERE COMM_ID=%r" %interactionID)
-    protocolname=cursor.fetchall()
-    db.close()
-    agentdef=(agentstates,protocolname[0][0])
-    return agentdef
+interdata_list=[]
+for j in interdata_raw[0]:
+    interdata_list.append(j)
 
-#find out how many lines in one agent state
-def getclauseset(agentstate):
-    pattern_n=re.compile(r'.*?\\n')
-    search_n=re.findall(pattern_n,agentstate)
-    return search_n
 
-interIDlist=getallinterID('localhost')
-print (interIDlist[0])
-agent_states=getintagentstates(interIDlist[0])
-
-def getagentinfo(agentinfoclause):
-    pattern_agentinfo=re.compile(r'a\(\s*(?P<agentrolename>\w*)\((?P<agentrolevars>.*)\),(?P<agentid>.*)\)::=\\n')
-    agent_role_name=re.search(pattern_agentinfo,agentinfoclause).group('agentrolename')
-    agent_role_vars=re.search(pattern_agentinfo,agentinfoclause).group('agentrolevars').split(",")
-    agent_id=re.search(pattern_agentinfo,agentinfoclause).group('agentid')
-    return (agent_role_name,agent_role_vars,agent_id)
-
-    #get message name and var
-def getmessageinfo(mbody):
-    pattern_msgdir=re.compile(r'(=>|<=)')
-    search_msgdir=re.findall(pattern_msgdir,mbody)
-    if search_msgdir:
-        if(search_msgdir[0]=="=>"):
-            pattern_msginfo=re.compile(r'.*c\(\s*(?P<mname>\w*)\((?P<mvars>.*)\)\s=>\s*a\((.*)\).*')
-            mvars=re.search(pattern_msginfo,mbody).group('mvars').split(",")
-            mname=re.search(pattern_msginfo,mbody).group('mname')
-            mdir="out"
-            pattern_mtarget=re.compile(r'.*c\(\s*(?P<mname>\w*)\((?P<mvars>.*)\)\s=>\s*a\(\s*(?P<mtarget>\w*)\(.*\),.*?\).*')
-            mtarget=re.search(pattern_mtarget,mbody).group('mtarget')
-            return (mname,mvars,mdir,mtarget)
-        else:
-            pattern_msginfo=re.compile(r'.*c\(\s*(?P<mname>\w*)\((?P<mvars>.*)\)\s<=.*')
-            mvars=re.search(pattern_msginfo,mbody).group('mvars').split(",")
-            mname=re.search(pattern_msginfo,mbody).group('mname')
-            mdir="in"
-            pattern_mtarget=re.compile(r'.*c\(\s*(?P<mname>\w*)\((?P<mvars>.*)\)\s<=\s*a\(\s*(?P<mtarget>\w*)\(.*\),.*?\).*')
-            mtarget=re.search(pattern_mtarget,mbody).group('mtarget')
-            return (mname,mvars,mdir,mtarget)
+new_interlist = []
+for i in range(len(interdata_list)):
+    if i != '':
+        new_interlist.append(interdata_list[i].replace('p12', 'p13'))
     else:
-        return False
+        new_interlist.append(interdata_list[i])
+
+insertquery = """INSERT INTO lccengine VALUES ('%s','%s','%s','%s','%s','%s','%s')""" % (new_interlist[0],
+                                                                                         new_interlist[1],
+                                                                                         new_interlist[2],
+                                                                                         new_interlist[3],
+                                                                                         new_interlist[4],
+                                                                                         new_interlist[5],
+                                                                                         new_interlist[6]
+                                                                           )
+
+print(insertquery)
+
+db = mysql.connector.connect(host="localhost",
+                             database='lccgame',
+                             user='host',
+                             password='host')
+cursor = db.cursor()
+cursor.execute(insertquery)
+db.close()
+
+
+
+
+
 
